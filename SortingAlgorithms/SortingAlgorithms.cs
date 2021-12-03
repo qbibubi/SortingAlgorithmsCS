0bce77ffe94a22ce685110e77c30115c5d0bdfb2
Qbibubi <58397100+Nantathu@users.noreply.github.com>
2021-12-03 18:40:38 +0100

    mergesort button

--- SortingAlgorithms/SortingAlgorithms.cs
+++ SortingAlgorithms/SortingAlgorithms.cs
@@ -6,6 +6,12 @@ namespace SortingAlgorithms
 {
     class SortingAlgorithms
     {
-
+        /* TODO LIST: 
+         * 1. Add bubble sorts (multiple perhaps)
+         * 2. Insertion sort
+         * 3. Quicksort
+         * 4. Heapsort
+         * 5. Mergesort
+         */
     }
 }
