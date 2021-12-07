c157548f2bb1a4cff916f1133651d4f01bf22959
Qbibubi <58397100+Nantathu@users.noreply.github.com>
2021-12-07 20:14:10 +0100

    only numeric input in textbox

--- SortingAlgorithms/MainWindow.xaml.cs
+++ SortingAlgorithms/MainWindow.xaml.cs
@@ -209,5 +209,20 @@ namespace SortingAlgorithms
 
             ElapsedTimeLabel.Content = "Elapsed time: " + stopwatch.ElapsedMilliseconds + "ms";
         }
+
+        private void NumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            int iValue = -1;
+
+            if (Int32.TryParse(textBox.Text, out iValue) == false)
+            {
+                TextChange textChange = e.Changes.ElementAt<TextChange>(0);
+                int iAddedLength = textChange.AddedLength;
+                int iOffset = textChange.Offset;
+
+                textBox.Text = textBox.Text.Remove(iOffset, iAddedLength);
+            }
+        }
     }
 }
