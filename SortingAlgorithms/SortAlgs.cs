746885101b7373f78218d2776caf0768b83d3811
Qbibubi <58397100+Nantathu@users.noreply.github.com>
2021-12-08 08:07:48 +0100

    heapsort

--- SortingAlgorithms/SortAlgs.cs
+++ SortingAlgorithms/SortAlgs.cs
@@ -96,5 +96,44 @@ namespace SortingAlgorithms
 
             return list;
         }
+
+        public static List<int> Heapify(List<int> list, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && list[left] > list[largest])
+                largest = left;
+
+            if (right < n && list[right] > list[largest])
+                largest = right;
+
+            if (largest != i)
+            {
+                int swap = list[i];
+                list[i] = list[largest];
+                list[largest] = swap;
+                Heapify(list, n, largest);
+            }
+
+            return list;
+        }
+
+        public static List<int> Heapsort(List<int> list, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(list, n, i);
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = list[0];
+                list[0] = list[i];
+                list[i] = temp;
+                Heapify(list, i, 0);
+            }
+
+            return list;
+        }
     }
 }
